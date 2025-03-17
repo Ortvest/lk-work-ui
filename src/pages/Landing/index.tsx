af94e1e66a1af126dbf927c6278df0139ac207c7
Serhii Schastlivtsev <serkeydies@gmail.com>
2025-03-17 21:59:37 +0200

    [LWK-5] feat: added global wrapper, fixed wrapper for header

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -2,10 +2,13 @@ import { Fragment } from 'react';
 
 import { Header } from '@modules/Header';
 
+import { GlobalContainer } from '@shared/components/GlobalContainer';
+
 export const LandingPage = (): JSX.Element => {
   return (
     <Fragment>
       <Header />
+      <GlobalContainer>Hello</GlobalContainer>
     </Fragment>
   );
 };
