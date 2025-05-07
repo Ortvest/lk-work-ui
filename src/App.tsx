f75c2d3eb91eb0e46b9defa1b3bb517aecac0331
Serhii Schastlivtsev <serkeydies@gmail.com>
2025-05-08 02:24:30 +0300

    [LWK-17] ref: fixed formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+
 import { RouterProvider } from 'react-router-dom';
 
 import { router } from '@global/router/router';
@@ -5,15 +7,15 @@ import { router } from '@global/router/router';
 import { useTypedSelector } from '@shared/hooks/useTypedSelector';
 
 import '@shared/config/style.config.css';
+
 import { useGetMeQuery } from '@global/api/auth/auth.api';
-import { useEffect } from "react";
 
 export const App = (): JSX.Element => {
-  const {refetch} = useGetMeQuery(undefined);
+  const { refetch } = useGetMeQuery(undefined);
 
   useEffect(() => {
-    refetch()
-  }, [])
+    refetch();
+  }, []);
   const authed = useTypedSelector((state) => state.userReducer.isAuth);
 
   const currentRouter = router(Boolean(authed), false);
