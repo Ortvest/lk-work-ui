5a313829e20e4a0bf0ff33a1a38d4b9b7b33d822
Dmytro Kazantsev <d.kazantsev@codejet.ai>
2025-08-17 12:12:53 +0200

    [LWK-52] feat: download docs

--- src/shared/components/SharedImagePreview/index.tsx
+++ src/shared/components/SharedImagePreview/index.tsx
@@ -1,7 +1,5 @@
 import classNames from 'classnames';
-
 import DownLoadIcon from '@shared/assets/icons/DownloadIcon.svg';
-
 import './style.css';
 
 interface SharedImagePreviewProps {
@@ -10,10 +8,15 @@ interface SharedImagePreviewProps {
 }
 
 export const SharedImagePreview = ({ imageName, imageUrl }: SharedImagePreviewProps): JSX.Element => {
+
   return (
     <label className={classNames('shared-img-preview-container')}>
       <div className={classNames('shared-img-preview-wrapper')}>
-        <img className={classNames('shared-img-preview')} src={imageUrl || ''} alt="document-photo" />
+        <img
+          className={classNames('shared-img-preview')}
+          src={imageUrl || ''}
+          alt="document-photo"
+        />
       </div>
       <div className={classNames('shared-img-preview-name-wrapper')}>
         <p className={classNames('shared-img-preview-name')}>{imageName}</p>
@@ -21,7 +24,15 @@ export const SharedImagePreview = ({ imageName, imageUrl }: SharedImagePreviewPr
           Size: {imageUrl ? (imageUrl.length / 1024).toFixed(2) + ' Mb' : '-'}
         </p>
       </div>
-      <img className={classNames('shared-img-preview-download-icon')} src={DownLoadIcon} alt="upload-icon" />
+      <img
+        className={classNames('shared-img-preview-download-icon')}
+        src={DownLoadIcon}
+        alt="download-icon"
+        onClick={(e) => {
+          e.stopPropagation();
+          window.open(imageUrl, '_blank');
+        }}
+      />
     </label>
   );
 };
