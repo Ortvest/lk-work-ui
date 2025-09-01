0eb28f7d711a5f1b5f95ca1b65436eafe84c4389
Dmytro Kazantsev <d.kazantsev@codejet.ai>
2025-09-01 22:12:05 +0200

    [LWK-X] feat: deploy fix

--- src/shared/components/LanguageSwitcherDropdown/index.tsx
+++ src/shared/components/LanguageSwitcherDropdown/index.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { useTranslation } from 'react-i18next';
 
 import './style.css';
 
-import { availableLanguages } from '@global/i18n/languages';
+import { availableLanguagesPromise } from '@global/i18n/languages';
 
 export const LanguageSwitcherDropdown = (): React.ReactNode => {
   const { i18n } = useTranslation();
+  const [availableLanguages, setAvailableLanguages] = useState<{ code: string; label: string }[]>([]);
 
+  // @ts-ignore
+  useEffect(() => {
+    let alive = true;
+    availableLanguagesPromise.then((langs) => alive && setAvailableLanguages(langs));
+    // @ts-ignore
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+    return () => {
+      alive = false;
+    };
+  }, []);
   const changeLanguage = async (code: string): Promise<void> => {
     await i18n.changeLanguage(code);
   };
